2c5db54e2ddb09d23a08901d1febde3cb6562537
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-10 00:57:03 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Чертоусова люби математику
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x, y;
+    float k, b;
+    cin >> x >> k >> b;
+    y = k * x + b;
+    cout << y << endl;
+    if (y > 0 && x > 0) cout << "1 четверть";
+    if (y > 0 && x < 0) cout << "2 четверть";
+    if (y < 0 && x < 0) cout << "3 четверть";
+    if (y < 0 && x>0) cout << "4 четверть";
+
+}
+
